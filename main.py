fc2ff7cd20fdbab1e1ae3574dbf5b15bca4101b3
PUDGE\welte <welter.tom@hotmail.com>
2022-11-06 00:28:04 +0100

    Added sort of a randomForest implementation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from solver import Solver
 import api
+import json
+
 
 api_key = "c5e9d57d-cd56-4a65-4817-08da97ce1ad5"   # TODO: Your api key here
 # The different map names can be found on considition.com/rules
@@ -8,22 +10,46 @@ map_name = "Fancyville"
 # TODO: You bag type choice here. Unless changed, the bag type 1 will be selected.
  
 
+
 def main():
-	for i in range(5):
-		print(i)
-		print("Starting game...")
-		response = api.mapInfo(api_key, map_name)
-		bag_type = i
-		days = 31 if map_name == "Suburbia" or map_name == "Fancyville" else 365
-		solver = Solver(game_info=response)
-		solution = solver.Solve(bag_type, days)
-		submit_game_response = api.submit_game(api_key, map_name, solution)
-		print(f'Result for map {map_name} with bag type {bag_type}')
-		if i !=0 :
-			print(submit_game_response.get('dailys')[-1])
-			print('Score = '+str(submit_game_response.get('dailys')[-1].get('customerScore') - submit_game_response.get('dailys')[-1].get('c02')))
-		else:
-			print(submit_game_response)
+	highscore = 0
+	runs = 100
+	recycle = [0,1]
+	bags = [1,2,3,4,5]
+	print("Starting game...")
+	response = api.mapInfo(api_key, map_name)
+	highscore, best_sol = run_logic(response, highscore, runs+1, bags, recycle)
+	print(f'Highscore: {highscore}')
+	print(best_sol.bagPrice, best_sol.bagType, best_sol.recycleRefundChoice, best_sol.refundAmount, best_sol.orders)
+	highscore, best_sol = run_logic(response, highscore, 100, [best_sol.bagType], [best_sol.recycleRefundChoice])
+	store_data(highscore, best_sol.toJSON())
+
+def run_logic(response, highscore, runs, bags, recycle):
+	for bag in bags:
+		for rec in recycle:
+				for iteration in range(1,runs):
+					try:
+						bag_type = bag
+						days = 31 if map_name == "Suburbia" or map_name == "Fancyville" else 365
+						solver = Solver(game_info=response)
+						solution = solver.Solve(bag_type, days, rec)
+						submit_game_response = api.submit_game(api_key, map_name, solution)
+						print(f'Result for map {map_name} with bag type {bag_type}')
+						print(submit_game_response.get('dailys')[-1])
+						score = submit_game_response.get('dailys')[-1].get('customerScore') - submit_game_response.get('dailys')[-1].get('c02')
+						if score > highscore:
+							highscore = score
+							best_sol = solution
+						print('Score = '+str(score))
+					except Exception:
+						pass
+	return highscore, best_sol
+
+def store_data(params, sol):
+	with open ("params.txt", "a") as f:
+		f.write(json.dumps(params))
+		f.write(json.dumps(sol))
+		f.write("\n")
 
 if __name__ == "__main__":
 	main()
--- solver.py
+++ solver.py
@@ -1,12 +1,12 @@
-import operator
-
+import random
 from solution import Solution
 
 
+
 class Solver:
-    bagType_price = [1.7, 1.75, 6, 25, 200]
-    bagType_co2_production = [5, 7, 3, 6, 20]
-    bagType_co2_transport = [50, 40, 60, 70, 100]
+    bagType_price = [0, 1.7, 1.75, 6, 25, 200]
+    bagType_co2_production = [0, 5, 7, 3, 6, 20]
+    bagType_co2_transport = [0, 50, 40, 60, 70, 100]
 
     def __init__(self, game_info):
         self.days = None
@@ -14,16 +14,32 @@ class Solver:
         self.companyBudget = game_info["companyBudget"]
         self.behavior = game_info["behavior"]
 
-    def Solve(self, bagtype, days):
-        self.days = days
-        solution = Solution(True, self.bagType_price[bagtype], 1, bagtype)
 
+    def Solve(self, bagtype, days, recycle):
+        self.days = days
+        refund = 1
+        solution = Solution(bool(recycle), self.bagType_price[bagtype], refund, bagtype)
         for day in range(0, days):
-            solution.addOrder(self.wasteMoney(bagtype))
-        
+            # if day%7 == 0:
+            #     solution.addOrder(self.wasteMoney(bagtype))
+            # else :
+            #     solution.addOrder(self.holdMoney(bagtype))
+            number = random.randint(0,4)
+            if number == 0:
+                solution.addOrder(self.splitMoney(bagtype))
+            elif number == 1:
+                solution.addOrder(self.holdMoney(bagtype))
+            elif number == 2:
+                solution.addOrder(self.cranked())
+            else : 
+                solution.addOrder(self.wasteMoney(bagtype))
         return solution
 
+    def rand_test(self):
+            return int(random.random()*self.companyBudget)
 
+    def cranked(self):
+        return int(self.companyBudget/self.days)
     # Solution 1: "Spend all money day 1"
     def wasteMoney(self, bagtype):
         return int(self.companyBudget / self.bagType_price[bagtype])
